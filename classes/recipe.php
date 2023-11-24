e86dfe613b7ce31c068595231cf88d3787eaad0b
JNBetteraij <95929522+JNBetteraij@users.noreply.github.com>
2023-11-24 09:16:16 +0100

    added Ingredients class: changes to recipe class and databaseManager class to include join table

--- classes/recipe.php
+++ classes/recipe.php
@@ -10,11 +10,11 @@ class Recipe implements Storable{
     private string $instructions;
     private array $ingredients;
 
-    function __construct(int $id, string $date, string $name, string $descirption, int $prepTime, string $instructions, array $ingredients){
+    function __construct(int $id, string $date, string $name, string $description, int $prepTime, string $instructions, array $ingredients){
         $this->id = $id;
         $this->date = $date;
         $this->name = $name;
-        $this->description = $descirption;
+        $this->description = $description;
         $this->prepTime = $prepTime;
         $this->instructions = $instructions;
         $this->ingredients = $ingredients;
@@ -24,7 +24,7 @@ class Recipe implements Storable{
         return $this->id;
     }
 
-    public function setID($id): int{
+    public function setID($id): void{
         $this->id = $id;
     }
 
@@ -32,7 +32,7 @@ class Recipe implements Storable{
         return $this->date;
     }
 
-    public function setDate($date): string{
+    public function setDate($date): void{
         $this->date = $date;
     }
 
@@ -40,7 +40,7 @@ class Recipe implements Storable{
         return $this->description;
     }
 
-    public function setDescription($description): string{
+    public function setDescription($description): void{
         $this->description = $description;
     }
 
@@ -48,7 +48,7 @@ class Recipe implements Storable{
         return $this->name;
     }
 
-    public function setName($name): string{
+    public function setName($name): void{
         $this->name = $name;
     }
 
@@ -56,11 +56,27 @@ class Recipe implements Storable{
         return $this->instructions;
     }
 
-    public function setInstructions($instructions): string{
+    public function setInstructions($instructions): void{
         $this->instructions = $instructions;
     }
 
-    public function convertToDatabaseFormat(){
+    public function convertToDatabaseFormat(): array{
         return get_object_vars($this);
+    }
+
+    public function getRecipeTableValues(): object {
+        $tableData = new stdClass();
+        $tableData->headers = ["recipe_name", "brief_description", "preparation_time", "instructions"];
+        $tableData->values = [ 
+            $this->inQuotes($this->name),
+            $this->inQuotes($this->description),
+            $this->prepTime,
+            $this->inQuotes($this->instructions)
+        ];
+        return $tableData;
+    }
+
+    private function inQuotes($text): string{
+        return "'" . trim($text) . "'";
     }
 }
--- database/databaseManager.php
+++ database/databaseManager.php
@@ -1,61 +1,178 @@
 <?php declare(strict_types = 1);
 
     include_once("../classes/recipe.php");
+    include_once("../classes/Ingredient.php");
     class DatabaseManager{
-
         private PDO $dbConnection;
-        private string $table = "recipes";
-        private array $columns = ["id", "date", "name", "brief_description", "preparation_time", "instructions"];
+
+        private string $username = "root";
+        private string $password = "";
+        private string $host = "localhost";
+        private string $dbname = "recipebook";
+
+        private string $recipeTableName = "recipes";
+        private string $ingredientTableName = "ingredients";
+        private string $joinTableName = "ingredients_recipes";
 
         public function __construct(){
-            $this->createConnection();
-        }
-
-        private function createConnection(){
-            $username = "root";
-            $password = "";
-            $host = "localhost";
-            $dbname = "recipebook";
-
-            $this->dbConnection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        }
-
-        public function addRecipe(Recipe $recipe): bool{
             try {
-                $values = $recipe->convertToDabaseFormat();
-                // $this->dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //will have to look up what this does
-                $statement = $this->dbConnection->query("INSERT INTO $this->table (implode(',',$this->columns))
-                VALUES (implode(',',$values))");
-                $statement->execute();
-                return true;
+                $this->dbConnection = $this->createDBConnection();
             } catch (PDOException $pdoException) {
-                return false;
+                //note: currently assuming db doesn't exist, therefor run setup for database
+                $this->setupDatabase();
+                $this->dbConnection = $this->createDBConnection();
             }
         }
 
-        public function deleteRecipe(int $id): bool{
-            try{
-                $statement = $this->dbConnection->query("DELETE FROM recipes WHERE id = $id");
+        private function createDBConnection(): PDO{
+            return new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+        }
+
+#region database set up
+        public function setupDatabase(){
+            $this->createDatabase();
+            $this->createRecipeTable();
+            $this->createIngredientTable();
+            $this->createRecipeIngredientJoinTable();
+        }
+
+        private function createDatabase(): void{
+            try {
+                $conn = new PDO("mysql:host=$this->host", $this->username, $this->password);
+                $conn->exec("CREATE DATABASE $this->dbname");
+                $conn = null;
+            } 
+            catch (PDOException $pdoException) {
+                //throw $pdoException;
+            }
+        }
+
+        public function createRecipeTable(): void{
+            try {
+                $conn = $this->createDBConnection();
+                $conn->exec("CREATE TABLE $this->recipeTableName(
+                    id INT(36) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    submission_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                    recipe_name VARCHAR(255),
+                    brief_description VARCHAR(255),
+                    preparation_time INT(11),
+                    instructions TEXT
+                )");
+                $conn = null;
+            } 
+            catch (PDOException $pdoException) {
+                //throw $pdoException;
+            }
+        }
+
+        public function createIngredientTable(): void{
+            try {
+                $conn = $this->createDBConnection();
+                $conn->exec("CREATE TABLE $this->ingredientTableName(
+                    id INT(36) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    ingredient_name VARCHAR(255),
+                    brief_description VARCHAR(255)
+                )");
+                $conn = null;
+            } 
+            catch (PDOException $pdoException) {
+                //throw $pdoException;
+            }
+        }
+
+        public function createRecipeIngredientJoinTable(): void{
+            try {
+                $conn = $this->createDBConnection();
+                $conn->exec("CREATE TABLE $this->joinTableName(
+                    recipe_id INT(36),
+                    ingredient_id INT(36),
+                    amount INT(36),
+                    units VARCHAR(255)
+                )");
+                $conn = null;
+            } 
+            catch (PDOException $pdoException) {
+                //throw $pdoException;
+            }
+        }
+#end region
+        
+#region add entries
+        public function addRecipe(Recipe $recipe): int|Exception{
+            try {
+                $recipeTableData = $recipe->getRecipeTableValues();
+                $columnHeaders = implode(',',$recipeTableData->headers);
+                $columnValues = implode(',',$recipeTableData->values);
+
+                echo "see if this runs<br>";
+
+                // $statement = $this->dbConnection->prepare("INSERT INTO $this->recipeTableName ($columnHeaders)
+                // VALUES($columnValues)");
+                
+                $statement = $this->dbConnection->prepare("INSERT INTO bestaat_niet ($columnHeaders)
+                VALUES($columnValues)");
+
                 $statement->execute();
-                return true;
-            }
-            catch (PDOException $pdoexception){
-                return false;
+                $id = $this->dbConnection->lastInsertId();
+                echo "added new recipe with id:$id<br>";
+                return (int)$id;
+            } 
+            catch (Exception $exception) {
+                echo "!exception thrown when adding new recipe<br>".$exception;
+                throw $exception;
+                echo "show this after exception<br>";
             }
         }
 
-        public function updateRecipe(int $id, Recipe $recipe): bool{
-            try{
-                $this->deleteRecipe($id);
-                $this->addRecipe($recipe);
+        public function addIngredient(Ingredient $ingredient): string|false{
+            try {
+                $tableData = $ingredient->convertToDatabaseFormat();
+                $columnHeaders = implode(',',$tableData->headers);
+                $columnValues = implode(',',$tableData->values);
+
+                $statement = $this->dbConnection->prepare("INSERT INTO $this->ingredientTableName ($columnHeaders)
+                VALUES($columnValues)");
+                $statement->execute();
+
+                $id = $this->dbConnection->lastInsertId();
+                if(is_bool($id) && !$id){
+                    
+                }
+                return $id;
+            } 
+            catch (PDOException $pdoException) {
+                echo $pdoException;
+                return null;
             }
-            catch (PDOException $pdoexception){
-                return false;
-            }
-            
-            return false;
         }
 
+        public function addIngredientToRecipe(Recipe $recipe, Ingredient $ingredient, bool $newIngredient = false){
+            try {
+                if($newIngredient){
+                    $this->addIngredient($ingredient);
+                    //TODO: get new ingredient entry ID
+                    //note need to ensure we retrieve the correct latest added
+                }
+
+                if(is_null($ingredient->getID())){
+                    throw new Exception("ingredient missing valid ID");
+                    return;
+                }
+            } 
+            catch (Exception $exception) {
+                //throw $exception;
+            }
+        }
+
+        private function addEntryTonRecipeIngredientJoinTable(Recipe $recipe,Ingredient $ingredient){
+            $recipeID = $recipe->getID();
+            $ingredientID = $ingredient->getID();
+
+
+        }
+#end region
+        
+#region read/get entries
         public function getRecipe(int $id): Recipe{
             $statement = $this->dbConnection->query("SELECT * FROM recipes WHERE id = $id");
             $statement->execute();
@@ -84,8 +201,6 @@
                 echo $recipe["name"]. "<br>";
                 echo $key . "<br>";
             }*/
-            
-    
         }
 
         private function createRecipeObject(array $data){
@@ -97,24 +212,85 @@
             $recipe = new Recipe(...$values);
             return $recipe;
         }
-        
+#end region
+
+#region update entries
+        public function updateRecipe(int $id, Recipe $recipe): void{
+            try {
+                $recipeTableData = $recipe->getRecipeTableValues();
+                $updatedColumnHeaders = $recipeTableData->headers;
+                $updatedColumnValues = $recipeTableData->values;
+                $this->updateEntryInTable($this->recipeTableName, $id, $updatedColumnHeaders, $updatedColumnValues);
+            } 
+            catch (Exception $exception) {
+                throw $exception;
+            }
+        }
+
+        public function updateIngredient(int $id, Ingredient $ingredient): void{
+            $tableData = $ingredient->convertToDatabaseFormat();
+                $updatedColumnHeaders = $tableData->headers;
+                $updatedColumnValues = $tableData->values;
+                $this->updateEntryInTable($this->recipeTableName, $id, $updatedColumnHeaders, $updatedColumnValues);
+        }
+
+        public function updateEntryInTable(string $tableName, int $id, array $columnHeaders, array $columnValues):void{
+            try{
+                $updates = [];
+                foreach ($columnHeaders as $key => $columnHeader) {
+                    $querySnippet = $columnHeader."=".$columnValues[$key];
+                    array_push($updates, $querySnippet);
+                }
+                $updates = implode( ", ",$updates);
+
+                $statement = $this->dbConnection->prepare("UPDATE $tableName SET $updates WHERE id=$id");
+                $statement->execute();
+            }
+            catch (PDOException $pdoException){
+                throw $pdoException;
+            }
+        }
+#end region
+
+#region delete
+        public function deleteRecipe(int $id): bool{
+            try{
+                //test if this works with prepare then delete this comment
+                $statement = $this->dbConnection->prepare("DELETE FROM $this->recipeTableName WHERE id = $id");
+                $statement->execute();
+                return true;
+            }
+            catch (PDOException $pdoException){
+                return false;
+            }
+        }
+
+        public function deleteEntryInTable(string $tableName, int $id): void{
+            try{
+                $statement = $this->dbConnection->prepare("DELETE FROM $tableName WHERE id = $id");
+                $statement->execute();
+            }
+            catch (PDOException $pdoException){
+                throw $pdoException;
+            }
+        }
+#end region
     }
+
+
     $dbm = new DatabaseManager();
-
-    // $recipes = $dbm->getAllRecipes();
-    // foreach($recipes as $key => $recipe){
-    //     echo $recipe->getName(). "<br>";
-    //     echo $key . "<br>";
-    // }
-    $recipe = $dbm->getRecipe(2);
-    echo var_dump($recipe->convertToDatabaseFormat());
+    echo "test error handling<br>";
+    $testRecipe = new Recipe(0, date("Y-m-d H:i:s"), "recipe title", "brief_description", 69, "instruction list",[]);
+    try {
+        $result = $dbm->addRecipe($testRecipe);
+        echo "result:".$result."<br><br>";
+        echo "$result is van type: ".gettype($result)."<br>";
+    } catch (\Throwable $th) {
+        echo "caught error <br><br>";
+    }
     
-    //echo var_export($dbm->deleteRecipe(1), true);
 
-
-    // echo var_dump($recipe);
-    // foreach($recipe as $key => $col){
-    //     echo "$col<br>";
-    // }
+    // $updateRecipe = new Recipe(0, date("Y-m-d H:i:s"), "updated recipe title", "updated description", 109, "new instruction list",[]);
+    // $dbm->updateRecipe(8,$updateRecipe);
     
 ?>
