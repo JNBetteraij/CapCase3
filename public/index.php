8bbe8e3c441787f9970b5157e21a3323693532b6
Djowie Snijders <djowie.snijders@gmail.com>
2023-12-01 11:53:27 +0100

    Added image to article and made minor adjustments

--- public/index.php
+++ public/index.php
@@ -6,7 +6,6 @@ require 'indexClasses.php';
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="index.css">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Receptenboek</title>   
